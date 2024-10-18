5c23d428b49379b7258ee170e3f821d9feb8a15a
Christian Cricchi <studychristiancricchi@gmail.com>
2024-10-18 05:29:24 +0000

    feat: added procfile.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn shelly-scorner.wsgi:application
--- shellys_corner/settings.py
+++ shellys_corner/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 if os.path.exists('env.py'):
     import env
 
@@ -25,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ev$30jp5j3#a26_due$$7w$*(^uax=ok#7r&s52u9@_u^2tpg'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-christiancr-shellyscorn-cf2szshhdh1.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-christiancr-shellyscorn-cf2szshhdh1.ws.codeinstitute-ide.net','shelly-scorner','localhost']
 
 
 # Application definition
@@ -123,12 +125,17 @@ WSGI_APPLICATION = 'shellys_corner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
