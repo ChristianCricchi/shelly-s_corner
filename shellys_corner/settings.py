e3af1e4e7de1c2b4e87c903176c5a3e499abd965
Christian Cricchi <studychristiancricchi@gmail.com>
2024-10-19 10:39:53 +0000

    Feat: removed secret key and set debag.

--- shellys_corner/settings.py
+++ shellys_corner/settings.py
@@ -27,11 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)ev$30jp5j3#a26_due$$7w$*(^uax=ok#7r&s52u9@_u^2tpg'
+#SECRET_KEY = 'django-insecure-)ev$30jp5j3#a26_due$$7w$*(^uax=ok#7r&s52u9@_u^2tpg'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'shellys-corner-5b60b12a8abf.herokuapp.com', 
