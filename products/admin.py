9bac59558bc62a7bf0fa4f9eb1873d46637e6db1
Christian Cricchi <studychristiancricchi@gmail.com>
2024-10-20 11:51:53 +0000

    feat(admin): set up admin interface for Reviews.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Review
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -18,7 +18,18 @@ class CategoryAdmin(admin.ModelAdmin):
         'friendly_name',
         'name',
     )
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'rating',
+        'details',
+        'submitted_by',
+        'date_submitted',
+    )
     
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Review, ReviewAdmin)
 
